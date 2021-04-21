ffa0587ceb4d668773c15e8ba3579765c981c9d5
Man88GG <opfuga@gmail.com>
2021-04-21 11:03:42 -0600

    Seguridad Agregada

---
+++ 2P PROGRAMA/SOLUCIONP2/CapaVista2P/MDI.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace CapaVista2P
+{
+    partial class MDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarSesionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.catalogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.departamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguridadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambioDeContraseñaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeAplicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeModuloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDePerfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignaciónDePermisosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.catalogoToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.informesToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.ayudasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1116, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cerrarSesionToolStripMenuItem});
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // cerrarSesionToolStripMenuItem
+            // 
+            this.cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
+            this.cerrarSesionToolStripMenuItem.Size = new System.Drawing.Size(169, 26);
+            this.cerrarSesionToolStripMenuItem.Text = "Cerrar Sesión";
+            this.cerrarSesionToolStripMenuItem.Click += new System.EventHandler(this.cerrarSesionToolStripMenuItem_Click);
+            // 
+            // catalogoToolStripMenuItem
+            // 
+            this.catalogoToolStripMenuItem.Name = "catalogoToolStripMenuItem";
+            this.catalogoToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.catalogoToolStripMenuItem.Text = "Catalogo";
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.departamentosToolStripMenuItem});
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // departamentosToolStripMenuItem
+            // 
+            this.departamentosToolStripMenuItem.Name = "departamentosToolStripMenuItem";
+            this.departamentosToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.departamentosToolStripMenuItem.Text = "Departamentos";
+            this.departamentosToolStripMenuItem.Click += new System.EventHandler(this.departamentosToolStripMenuItem_Click);
+            // 
+            // informesToolStripMenuItem
+            // 
+            this.informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            this.informesToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.informesToolStripMenuItem.Text = "Informes";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seguridadToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(100, 24);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            this.seguridadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cambioDeContraseñaToolStripMenuItem,
+            this.mantenimientoDeUsuariosToolStripMenuItem,
+            this.mantenimientoDeAplicacionesToolStripMenuItem,
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem,
+            this.mantenimientoDeModuloToolStripMenuItem,
+            this.perfilToolStripMenuItem,
+            this.asignaciónDePermisosToolStripMenuItem});
+            this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.seguridadToolStripMenuItem.Text = "Seguridad";
+            // 
+            // cambioDeContraseñaToolStripMenuItem
+            // 
+            this.cambioDeContraseñaToolStripMenuItem.Name = "cambioDeContraseñaToolStripMenuItem";
+            this.cambioDeContraseñaToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.cambioDeContraseñaToolStripMenuItem.Text = "Cambio de Contraseña";
+            this.cambioDeContraseñaToolStripMenuItem.Click += new System.EventHandler(this.cambioDeContraseñaToolStripMenuItem_Click);
+            // 
+            // mantenimientoDeUsuariosToolStripMenuItem
+            // 
+            this.mantenimientoDeUsuariosToolStripMenuItem.Name = "mantenimientoDeUsuariosToolStripMenuItem";
+            this.mantenimientoDeUsuariosToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.mantenimientoDeUsuariosToolStripMenuItem.Text = "Mantenimiento de Usuarios";
+            this.mantenimientoDeUsuariosToolStripMenuItem.Click += new System.EventHandler(this.mantenimientoDeUsuariosToolStripMenuItem_Click);
+            // 
+            // mantenimientoDeAplicacionesToolStripMenuItem
+            // 
+            this.mantenimientoDeAplicacionesToolStripMenuItem.Name = "mantenimientoDeAplicacionesToolStripMenuItem";
+            this.mantenimientoDeAplicacionesToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.mantenimientoDeAplicacionesToolStripMenuItem.Text = "Mantenimiento de Aplicaciones";
+            this.mantenimientoDeAplicacionesToolStripMenuItem.Click += new System.EventHandler(this.mantenimientoDeAplicacionesToolStripMenuItem_Click);
+            // 
+            // asignacionDePerfilYAplicacionesToolStripMenuItem
+            // 
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem.Name = "asignacionDePerfilYAplicacionesToolStripMenuItem";
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem.Text = "Asignacion de perfil y aplicaciones";
+            this.asignacionDePerfilYAplicacionesToolStripMenuItem.Click += new System.EventHandler(this.asignacionDePerfilYAplicacionesToolStripMenuItem_Click);
+            // 
+            // mantenimientoDeModuloToolStripMenuItem
+            // 
+            this.mantenimientoDeModuloToolStripMenuItem.Name = "mantenimientoDeModuloToolStripMenuItem";
+            this.mantenimientoDeModuloToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.mantenimientoDeModuloToolStripMenuItem.Text = "Mantenimiento de Modulo";
+            this.mantenimientoDeModuloToolStripMenuItem.Click += new System.EventHandler(this.mantenimientoDeModuloToolStripMenuItem_Click);
+            // 
+            // perfilToolStripMenuItem
+            // 
+            this.perfilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mantenimientoDePerfilesToolStripMenuItem,
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem});
+            this.perfilToolStripMenuItem.Name = "perfilToolStripMenuItem";
+            this.perfilToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.perfilToolStripMenuItem.Text = "Perfil";
+            // 
+            // mantenimientoDePerfilesToolStripMenuItem
+            // 
+            this.mantenimientoDePerfilesToolStripMenuItem.Name = "mantenimientoDePerfilesToolStripMenuItem";
+            this.mantenimientoDePerfilesToolStripMenuItem.Size = new System.Drawing.Size(305, 26);
+            this.mantenimientoDePerfilesToolStripMenuItem.Text = "Mantenimiento de Perfiles";
+            this.mantenimientoDePerfilesToolStripMenuItem.Click += new System.EventHandler(this.mantenimientoDePerfilesToolStripMenuItem_Click);
+            // 
+            // asignacionDeAplicacionesAPerfilesToolStripMenuItem
+            // 
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem.Name = "asignacionDeAplicacionesAPerfilesToolStripMenuItem";
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem.Size = new System.Drawing.Size(305, 26);
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem.Text = "Asignacion de aplicaciones a perfiles";
+            this.asignacionDeAplicacionesAPerfilesToolStripMenuItem.Click += new System.EventHandler(this.asignacionDeAplicacionesAPerfilesToolStripMenuItem_Click);
+            // 
+            // asignaciónDePermisosToolStripMenuItem
+            // 
+            this.asignaciónDePermisosToolStripMenuItem.Name = "asignaciónDePermisosToolStripMenuItem";
+            this.asignaciónDePermisosToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.asignaciónDePermisosToolStripMenuItem.Text = "Asignación de Permisos";
+            this.asignaciónDePermisosToolStripMenuItem.Click += new System.EventHandler(this.asignaciónDePermisosToolStripMenuItem_Click);
+            // 
+            // ayudasToolStripMenuItem
+            // 
+            this.ayudasToolStripMenuItem.Name = "ayudasToolStripMenuItem";
+            this.ayudasToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.ayudasToolStripMenuItem.Text = "Ayudas";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Enabled = false;
+            this.txtUsuario.Location = new System.Drawing.Point(455, 3);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.ReadOnly = true;
+            this.txtUsuario.Size = new System.Drawing.Size(159, 22);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // MDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 450);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MDI";
+            this.Text = "MDI";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MDI_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem catalogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem departamentosToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.ToolStripMenuItem seguridadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambioDeContraseñaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeAplicacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignacionDePerfilYAplicacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeModuloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perfilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDePerfilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignacionDeAplicacionesAPerfilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignaciónDePermisosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarSesionToolStripMenuItem;
+    }
+}
---
+++ 2P PROGRAMA/SOLUCIONP2/CapaVista2P/frmMantenimientoDepto.designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace CapaVista
+{
+    partial class frmMantenimientoDepto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador1 = new CapaVistaNavegador.Navegador();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.rbtnInactivo = new System.Windows.Forms.RadioButton();
+            this.rbtnActivo = new System.Windows.Forms.RadioButton();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtNombreDepa = new System.Windows.Forms.TextBox();
+            this.txtIDepartamento = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.dgvDepartamentos = new System.Windows.Forms.DataGridView();
+            this.lblIDepa = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartamentos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.Color.Transparent;
+            this.navegador1.Location = new System.Drawing.Point(-2, 13);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(4);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(1412, 129);
+            this.navegador1.TabIndex = 1;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(478, 423);
+            this.txtEstado.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(33, 22);
+            this.txtEstado.TabIndex = 41;
+            this.txtEstado.Tag = "estado";
+            this.txtEstado.Visible = false;
+            this.txtEstado.TextChanged += new System.EventHandler(this.txtEstado_TextChanged);
+            // 
+            // rbtnInactivo
+            // 
+            this.rbtnInactivo.AutoSize = true;
+            this.rbtnInactivo.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnInactivo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rbtnInactivo.Location = new System.Drawing.Point(257, 402);
+            this.rbtnInactivo.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnInactivo.Name = "rbtnInactivo";
+            this.rbtnInactivo.Size = new System.Drawing.Size(100, 24);
+            this.rbtnInactivo.TabIndex = 40;
+            this.rbtnInactivo.TabStop = true;
+            this.rbtnInactivo.Text = "Inactivo";
+            this.rbtnInactivo.UseVisualStyleBackColor = true;
+            this.rbtnInactivo.CheckedChanged += new System.EventHandler(this.rbtnInactivo_CheckedChanged);
+            // 
+            // rbtnActivo
+            // 
+            this.rbtnActivo.AutoSize = true;
+            this.rbtnActivo.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnActivo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rbtnActivo.Location = new System.Drawing.Point(148, 402);
+            this.rbtnActivo.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnActivo.Name = "rbtnActivo";
+            this.rbtnActivo.Size = new System.Drawing.Size(84, 24);
+            this.rbtnActivo.TabIndex = 39;
+            this.rbtnActivo.TabStop = true;
+            this.rbtnActivo.Text = "Activo";
+            this.rbtnActivo.UseVisualStyleBackColor = true;
+            this.rbtnActivo.CheckedChanged += new System.EventHandler(this.rbtnActivo_CheckedChanged);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblEstado.Location = new System.Drawing.Point(74, 367);
+            this.lblEstado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(224, 20);
+            this.lblEstado.TabIndex = 38;
+            this.lblEstado.Text = "Estado del Departamento";
+            // 
+            // txtNombreDepa
+            // 
+            this.txtNombreDepa.Location = new System.Drawing.Point(78, 277);
+            this.txtNombreDepa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombreDepa.Name = "txtNombreDepa";
+            this.txtNombreDepa.Size = new System.Drawing.Size(333, 22);
+            this.txtNombreDepa.TabIndex = 37;
+            this.txtNombreDepa.Tag = "nombreDepartamento";
+            // 
+            // txtIDepartamento
+            // 
+            this.txtIDepartamento.Location = new System.Drawing.Point(78, 198);
+            this.txtIDepartamento.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIDepartamento.Name = "txtIDepartamento";
+            this.txtIDepartamento.Size = new System.Drawing.Size(333, 22);
+            this.txtIDepartamento.TabIndex = 36;
+            this.txtIDepartamento.Tag = "idDepartamentoEmpresa";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNombre.Location = new System.Drawing.Point(74, 253);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(236, 20);
+            this.lblNombre.TabIndex = 33;
+            this.lblNombre.Text = "Nombre del Departamento";
+            // 
+            // dgvDepartamentos
+            // 
+            this.dgvDepartamentos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDepartamentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDepartamentos.Location = new System.Drawing.Point(520, 170);
+            this.dgvDepartamentos.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvDepartamentos.Name = "dgvDepartamentos";
+            this.dgvDepartamentos.RowHeadersWidth = 51;
+            this.dgvDepartamentos.Size = new System.Drawing.Size(612, 277);
+            this.dgvDepartamentos.TabIndex = 35;
+            // 
+            // lblIDepa
+            // 
+            this.lblIDepa.AutoSize = true;
+            this.lblIDepa.Enabled = false;
+            this.lblIDepa.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIDepa.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblIDepa.Location = new System.Drawing.Point(75, 157);
+            this.lblIDepa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIDepa.Name = "lblIDepa";
+            this.lblIDepa.Size = new System.Drawing.Size(157, 20);
+            this.lblIDepa.TabIndex = 34;
+            this.lblIDepa.Text = "ID Departamento";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(79, 329);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(332, 22);
+            this.txtDescripcion.TabIndex = 42;
+            this.txtDescripcion.Tag = "descripcion";
+            // 
+            // frmMantenimientoDepto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(1423, 491);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.rbtnInactivo);
+            this.Controls.Add(this.rbtnActivo);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txtNombreDepa);
+            this.Controls.Add(this.txtIDepartamento);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.dgvDepartamentos);
+            this.Controls.Add(this.lblIDepa);
+            this.Controls.Add(this.navegador1);
+            this.Name = "frmMantenimientoDepto";
+            this.Text = "frmMantenimientoDepto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDepartamentos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CapaVistaNavegador.Navegador navegador1;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.RadioButton rbtnInactivo;
+        private System.Windows.Forms.RadioButton rbtnActivo;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtNombreDepa;
+        private System.Windows.Forms.TextBox txtIDepartamento;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.DataGridView dgvDepartamentos;
+        private System.Windows.Forms.Label lblIDepa;
+        private System.Windows.Forms.TextBox txtDescripcion;
+    }
+}
